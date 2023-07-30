42179eaec8bf5805776593e14689ee701f34d0c7
Jeon Hojin <ghwls030306@naver.com>
2023-07-30 17:24:39 +0900

    fix : ctx가 없는 경우 discord.errors.NotFound가 발생하는 오류 수정

--- Cogs/info.py
+++ Cogs/info.py
@@ -29,7 +29,13 @@ class info(commands.Cog):
         embed.add_field(name="보낸 내용",value=ctx.message,inline=False)
         embed.set_footer(text=f'하늘봇 버전 {self.bot.hanul_ver}')
         embed.set_image(url='https://i.ibb.co/7KNJZh4/lapis10-mp4-snapshot-08-40-2023-05-26-01-52-32.png')
-        await ctx.respond('아무래도 바보토끼가 또 바보토끼 한 것 같아요. 하토를 불러주세요!',embed=embed)
+        try:
+            await ctx.respond('아무래도 바보토끼가 또 바보토끼 한 것 같아요. 하토를 불러주세요!',embed=embed)
+        except discord.NotFound:
+            channel = await self.bot.fetch_channel(1126893408892502028)
+            channel.send('아무래도 바보토끼가 또 바보토끼 한 것 같아요. 하토를 불러주세요!',embed=embed)
+        except Exception as e:
+            raise e
         raise error
     
     
