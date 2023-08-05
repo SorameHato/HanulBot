1c9f196f8788a6416867e90bc02950eae94146eb
Jeon Hojin <ghwls030306@naver.com>
2023-08-05 15:48:31 +0900

    change : 중복코드 제거, 분류방법 변경

--- Cogs/expFrontEnd.py
+++ Cogs/expFrontEnd.py
@@ -134,12 +134,20 @@ class expFrontEnd(commands.Cog):
     async def rank(self,ctx,todayOrder:discord.Option(int,'누적 순위를 표시할지 오늘 올린 점수 순위만 표시할 지 선택해주세요!',name='정렬',choices=[discord.OptionChoice(name='누적 순위',value=0),discord.OptionChoice(name='오늘 순위',value=1),discord.OptionChoice(name='어제 순위',value=2)],default=0),arg:discord.Option(int,'몇 등까지 표시할 지 입력해주세요!', name='등수', choices=[discord.OptionChoice('5등',5),discord.OptionChoice('10등',10),discord.OptionChoice('전체',value=0)],default=0),isModern:discord.Option(int,'출력 스타일을 선택해주세요!',name='스타일',choices=[discord.OptionChoice(name='모던',value=1),discord.OptionChoice(name='텍스트',value=0)],default=1)):
         if todayOrder==2:
             yesterday = True
+            todayOrder = True
         else:
             yesterday = False
         if isModern:
-            await ctx.respond(f'활동점수 랭킹 현황이에요!```{await self.__showRanking__(ctx.guild,1,arg,modern=True,todayOrder=True,yesterday=yesterday)}```')
+            respond = f'활동점수 랭킹 현황이에요!```{await self.__showRanking__(1,arg,modern=True,todayOrder=todayOrder,yesterday=yesterday)}```'
         else:
-            await ctx.respond(f'활동점수 랭킹 현황이에요!{await self.__showRanking__(ctx.guild,1,arg,todayOrder=True,yesterday=yesterday)}')
+            respond = f'활동점수 랭킹 현황이에요!{await self.__showRanking__(1,arg,todayOrder=todayOrder,yesterday=yesterday)}'
+        try:
+            await ctx.respond(respond)
+        except discord.errors.NotFound:
+            channel = self.bot.get_channel(ctx.channel.id)
+            await channel.send(respond + f'\n<@{ctx.author.id}>인터렉션이 중간에 닫히는 문제가 발생해 별도의 채팅으로 전송했어요!')
+        except Exception as e:
+            raise e
 
 
 def setup(bot):
