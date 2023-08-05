4b281e2fcbe62c06c8358f3d457270ffdff6f6da
Jeon Hojin <ghwls030306@naver.com>
2023-08-05 15:50:40 +0900

    change : 구름IDE에서 수정하는 동안 바뀐 버전번호 반영

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pathlib
 from SkyLib import tui
 intents = discord.Intents.all()
 bot = discord.Bot(intents=intents)
-hanul_ver = "0.0 rev 117 (2023-07-31 01:42)"
+hanul_ver = "0.0 rev 128 (2023-08-03 10:42)"
 guild_ids = [
     1030056186915082262, #테스트용 서버
     1126790936723210290 #스카이형 서버
