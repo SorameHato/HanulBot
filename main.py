12a8fd10b07b1401da5bdb279fad2befb4e5fa7b
Jeon Hojin <ghwls030306@naver.com>
2023-08-10 00:45:23 +0900

    change : 버전 번호 올림

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pathlib
 from SkyLib import tui
 intents = discord.Intents.all()
 bot = discord.Bot(intents=intents)
-hanul_ver = "0.0 rev 167 (2023-08-09 15:29)"
+hanul_ver = "0.0 rev 168 (2023-08-10 00:30)"
 guild_ids = [
     1030056186915082262, #테스트용 서버
     1126790936723210290 #스카이형 서버
