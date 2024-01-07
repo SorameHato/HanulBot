ed1b5d9af2ef0667a2add13ab17548477a4fab7f
Hato Sorame <36477282+SorameHato@users.noreply.github.com>
2024-01-07 22:29:08 +0900

    Fix: Runtime Error (2)

--- Cogs/role.py
+++ Cogs/role.py
@@ -168,7 +168,7 @@ class giveRole(commands.Cog):
                 adminRole = ctx.guild.get_role(1126793481151598663)
                 ownerRole = ctx.guild.get_role(1126793415728824372)
             except Exception as e:
-                embed = discord.Embed(title=f'{message.member}님께 역할을 부여하는 동안 오류가 발생했어요!',description=f'권한 체크를 위해 어드민, 주인장 역할을 불러오던 중 오류가 발생했어요. 두 역할이 전부 존재하는지, 하늘봇이 읽을 수 있는지 확인해주세요!\n오류 내용 : {e}',color=self.bot.hanul_color)
+                embed = discord.Embed(title=f'{message.author}님께 역할을 부여하는 동안 오류가 발생했어요!',description=f'권한 체크를 위해 어드민, 주인장 역할을 불러오던 중 오류가 발생했어요. 두 역할이 전부 존재하는지, 하늘봇이 읽을 수 있는지 확인해주세요!\n오류 내용 : {e}',color=self.bot.hanul_color)
                 embed.add_field(name='역할을 부여한 관리자',value=ctx.user,inline=False)
                 embed.set_footer(text=f'하늘봇 버전 {self.bot.hanul_ver}')
                 await ctx.respond(embed=embed)
@@ -180,13 +180,13 @@ class giveRole(commands.Cog):
                 # 1126793565612281926 : 즐거운 게이머
                 # 1126878856582807592 : 서버 가이드 & 개발자
                 if adminRole in ctx.user.roles or ownerRole in ctx.user.roles:
-                    status, embed_r, debug = await self.add_role(ctx.guild,message.member,ctx.user)
+                    status, embed_r, debug = await self.add_role(ctx.guild,message.author,ctx.user)
                     match status:
                         case -2 | -1:
                             await ctx.respond(embed=embed_r)
                         case 0:
                             mainChannel = self.bot.get_channel(1126792316003307670)
-                            await mainChannel.send(f'<@{message.member.id}>',embed=embed_r)
+                            await mainChannel.send(f'<@{message.author.id}>',embed=embed_r)
                             await ctx.respond(debug)
                         case 1:
                             await ctx.respond(debug)
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import platform
 from SkyLib import tui
 intents = discord.Intents.all()
 bot = discord.Bot(intents=intents)
-hanul_ver = "0.0 rev 186 (2024-01-07 01:21)"
+hanul_ver = "0.0 rev 187 (2024-01-07 22:27)"
 guild_ids = [
     1030056186915082262, #테스트용 서버
     1126790936723210290 #스카이형 서버
